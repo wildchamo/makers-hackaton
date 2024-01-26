370c0002f43aaaefa845d3c3e6071e2170433cfb
Jacobo4 <juanjacoboib@gmail.com>
2024-01-25 21:00:49 -0500

    hotfixes

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,13 +1,43 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine, getOrCreateChat } from "react-chat-engine";
+import {useState} from "react";
+
 
 function Chat() {
+  const [username, setUsername] = useState("");
+
+  function createDirectChat(creds) {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  }
+
+  function renderChatForm(creds) {
+    return (
+      <div>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={() => createDirectChat(creds)}>Create</button>
+      </div>
+    );
+  }
+
   return (
-      <h1>chat</h1>
-    // <ChatEngine
-    //   projectID="00000000-0000-0000-0000-000000000000"
-    //   userName="adam"
-    //   userSecret="pass1234"
-    // />
+    <main>
+      <ChatEngine
+        height="100vh"
+        padding="0"
+        projectID={import.meta.env.VITE_REACT_APP_PROJECT_ID}
+        userName={import.meta.env.VITE_REACT_APP_USERNAME}
+        userSecret={import.meta.env.VITE_REACT_APP_USER_SECRET}
+        renderNewChatForm={(creds) => renderChatForm(creds)}
+
+      />
+    </main>
   );
 }
 
