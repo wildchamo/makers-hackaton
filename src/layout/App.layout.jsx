ecd8b5747cbf55faae9b97a3b214a5be735d92e7
Jacobo4 <juanjacoboib@gmail.com>
2024-01-25 20:51:36 -0500

    First config router, styles, etc

---
+++ src/layout/App.layout.jsx
@@ -0,0 +1,21 @@
+// Router
+import {Outlet} from "react-router-dom";
+
+// Components
+import Header from "./Header/Header.layout";
+import Footer from "./Footer/Footer.layout.jsx";
+
+const AppLayout = () => {
+
+    return (
+        <>
+            <div className="App">
+                <Header/>
+                <Outlet/>
+                <Footer/>
+            </div>
+        </>
+    );
+};
+
+export default AppLayout;
---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+
+function Chat() {
+  return (
+      <h1>chat</h1>
+    // <ChatEngine
+    //   projectID="00000000-0000-0000-0000-000000000000"
+    //   userName="adam"
+    //   userSecret="pass1234"
+    // />
+  );
+}
+
+export default Chat;
