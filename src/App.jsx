11dcadda4a5cba5c939e2ef16665c6c180afa058
Jose Luis <wildchamo@gmail.com>
2024-01-25 19:58:28 -0500

    update .env.local

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-holaa
-    </>
-  )
+    <ChatEngine
+      projectID="00000000-0000-0000-0000-000000000000"
+      userName="adam"
+      userSecret="pass1234"
+    />
+  );
 }
 
-export default App
+export default App;
