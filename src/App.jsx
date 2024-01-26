a32ad356c1c2829e0801522aed240190a95c81e7
Jose Luis <wildchamo@gmail.com>
2024-01-25 20:09:40 -0500

    enviroment varaibles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 
 function App() {
+
+  console.log(import.meta.env.VITE_REACT_APP_PROJECT_ID);
+
   return (
-    <ChatEngine
-      projectID="00000000-0000-0000-0000-000000000000"
-      userName="adam"
-      userSecret="pass1234"
-    />
+    <p>hola</p>
+    // <ChatEngine
+    //   projectID={process.env.REACT_APP_PROJECT_ID}
+    //   userName={process.env.REACT_APP_USER_NAME}
+    //   userSecret={process.env.REACT_APP_USER_SECRET}
+    // />
   );
 }
 
